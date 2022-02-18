5341102a4aca60d7886b28845b89c41f1fcea153
Guillaume Coré <gucore@redhat.com>
2022-02-18 16:21:54 +0100

    Move import down

--- lookup_plugins/test.py
+++ lookup_plugins/test.py
@@ -2,7 +2,6 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import (absolute_import, division, print_function)
-from ansible.plugins.filter.core import to_bool
 __metaclass__ = type
 
 DOCUMENTATION = """
@@ -29,6 +28,7 @@ _raw:
 """
 
 from ansible.plugins.lookup import LookupBase
+from ansible.plugins.filter.core import to_bool
 
 class LookupModule(LookupBase):
 
